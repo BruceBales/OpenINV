82be836a4c34c374542170ad997ac02be31f5585
Bruce Bales <bbales@kochava.com>
2016-11-13 22:18:15 -0800

    OpenINV - Add Security Functionality
    Added the ability to specify a private key to be used for a security token.
    
    Algorithm is sha256(private_token + item)

--- main.js
+++ main.js
@@ -2,13 +2,18 @@
 var argv    =     require('yargs');
 var http    =     require('http');
 var url     =     require('url');
-var actions  =     require('./lib/actions.js');
+var actions =     require('./lib/actions.js');
+var auth    =     require('./lib/auth.js');
 
 http.createServer(function(request, response) {
   var querystring = url.parse(request.url, true).query;
   console.log(querystring);
-
   var action = querystring.action;
+  var security_token = querystring.security_token;
+  is_authenticated = auth.Authenticate(querystring.item, security_token);
+  if (is_authenticated == false) {
+    response.end("Authentication Failed");
+  }
   switch(action) {
     case 'add':
       actions.Add(request);
